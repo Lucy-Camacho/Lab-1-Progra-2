0e5924197dba6499a0fd8e3a161c5e023779e55a
unknown <luxc1607@gmail.com>
2022-04-05 16:14:00 -0600

    Tercer Ejercicio

--- Ejercicios.cpp
+++ Ejercicios.cpp
@@ -22,11 +22,22 @@ void conversion (float miles, float gallons)
     cout << "La eficiencia del carro es: " << eficienciaMiGal << "Mi/Gal (equivalente a: " << eficienciaLtrKm << "L/100Km)." << endl;
 }
 
+void heightConversion (float feet, float inches)
+{
+    float ft2M= feet/3.281;
+    float in2M= inches/39.37;
+
+    float heightCon= ft2M+in2M;
+
+    cout << "La altura es: " << feet << " pies con " << inches << " pulgada(s). Y en metros: " << heightCon << "m" << endl;
+
+}
+
 int main()
 {
     atwood(16, 18);
     conversion(22, 350);
-
+    heightConversion(6, 1);
     
     return 0;
 }
