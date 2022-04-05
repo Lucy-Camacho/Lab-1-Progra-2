808e4e8eeb08ab28575d4fe60c196f609c3525ce
unknown <luxc1607@gmail.com>
2022-04-05 09:35:40 -0600

    Primer Ejercicio

--- Ejercicios.cpp
+++ Ejercicios.cpp
@@ -4,6 +4,14 @@ using namespace std;
 
 int main()
 {
-    return 0
+    return 0;
     
+}
+
+void atwood (float m1, float m2)
+{
+    const float gravedad= 9.8;
+
+    cout<< "Atwood: " << ((2*m1*m2)/(m1+m2))*gravedad << endl;
+
 }
