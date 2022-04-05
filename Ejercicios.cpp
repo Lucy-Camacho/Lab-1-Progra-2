981645ffc433af700d063418620ec3a8060cf5d7
unknown <luxc1607@gmail.com>
2022-04-05 16:49:49 -0600

    Segundo Ejercicio

--- Ejercicios.cpp
+++ Ejercicios.cpp
@@ -2,16 +2,34 @@
 
 using namespace std;
 
-int main()
-{
-    return 0;
-    
-}
 
 void atwood (float m1, float m2)
 {
     const float gravedad= 9.8;
 
-    cout<< "Atwood: " << ((2*m1*m2)/(m1+m2))*gravedad << endl;
+    cout<< "Tension= " << ((2*m1*m2)/(m1+m2))*gravedad << endl;
 
 }
+
+void conversion (float miles, float gallons)
+{
+    const float gal2Ltr= 3.785;
+    float galConversion= gallons*gal2Ltr;
+    
+    float eficienciaMiGal= miles/gallons;
+    float eficienciaLtrKm= gal2Ltr/100;
+
+    cout << "La eficiencia del carro es: " << eficienciaMiGal << "Mi/Gal (equivalente a: " << eficienciaLtrKm << "L/100Km)." << endl;
+}
+
+int main()
+{
+    atwood(16, 18);
+    conversion(22, 350);
+
+    
+    return 0;
+}
+
+
+
